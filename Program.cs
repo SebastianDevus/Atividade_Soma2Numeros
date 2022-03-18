96e1da187cc2c7fbec3f6063413c8e890f2d24f6
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-18 20:27:39 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int nr1, nr2, resultado;
+
+Console.WriteLine("Soma entre Números\n");
+
+Console.Write("Por favor, digite o primeiro número: ");
+nr1 = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Por favor, digite o segundo número: ");
+nr2 = Convert.ToInt32(Console.ReadLine()!);
+
+resultado = nr1 + nr2;
+
+Console.WriteLine($"O resultado é: {resultado}");
